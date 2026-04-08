d24692f7d4b3ec25b1e2b378999177af999859f5
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-08 17:17:16 -0400

    refactor to always close email after sending

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ load_dotenv()
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Create initial connection
-connection = smtplib.SMTP('smtp.gmail.com', 587)
-# Start transport layer security
-connection.starttls()
-# Obscure username and password from GitHub public repo
-# Retrieve .env variables
-outgoing_email = os.getenv("SMTP_USER_OUTGOING_EMAIL")
-outgoing_email_password = os.getenv("SMTP_USER_OUTGOING_EMAIL_APP_PASS")
-receiving_email = os.getenv("SMTP_USER_RECEIVING_EMAIL")
-connection.login(user=outgoing_email, password=outgoing_email_password)
-# Send message
-message = f"Hello Ryan this a test of the smtplib in python"
-connection.sendmail(from_addr=outgoing_email, to_addrs=receiving_email, msg=message)
-connection.close()
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    # Start transport layer security
+    connection.starttls()
+    # Obscure username and password from GitHub public repo
+    # Retrieve .env variables
+    outgoing_email = os.getenv("SMTP_USER_OUTGOING_EMAIL")
+    outgoing_email_password = os.getenv("SMTP_USER_OUTGOING_EMAIL_APP_PASS")
+    receiving_email = os.getenv("SMTP_USER_RECEIVING_EMAIL")
+    connection.login(user=outgoing_email, password=outgoing_email_password)
+    # Send message
+    message = f"Subject: Email_SMTPLIB_TEST\n\n Hello Ryan, \n\nThis a test of the smtplib in python with a subject"
+    connection.sendmail(from_addr=outgoing_email, to_addrs=receiving_email, msg=message)
